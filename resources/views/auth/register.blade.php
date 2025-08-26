23037fd350fea5f7e858f825c9a6c0023480b519
Junior <juniorod99@gmail.com>
2025-08-26 15:11:58 -0300

    Registrando um usuário

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,17 +1,28 @@
 <div>
-    <h1>Login</h1>
+    <h1>Register</h1>
 
     @if($message = session()->get('message'))
         <div>{{ $message }}</div>
     @endif
 
-    <form action="{{ route('login') }}" method="post">
+    <form action="{{ route('register') }}" method="post">
         @csrf
+        <div>
+            <input name="name" placeholder="Name" value="{{ old('name') }}">
+            @error('name')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+        <br>
         <div>
             <input name="email" placeholder="Email" value="{{ old('email') }}">
             @error('email')
                 <span>{{ $message }}</span>
             @enderror
+        </div>
+        <br>
+         <div>
+            <input name="email_confirmation" placeholder="Email Confirmation" >
         </div>
         <br>
         <div>
@@ -21,6 +32,6 @@
             @enderror
         </div>
         <br>
-        <button>Logar</button>
+        <button>Registrar</button>
     </form>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [registerController::class, 'register']);
+
 Route::get('/dashboard', fn() => 'dashboard :: ' . auth()->id())->middleware('auth')->name('dashboard');
